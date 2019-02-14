c69854b909a84775936c914d86142028d5ba4ad6
Jeremy Metha <jeremymetha@gmail.com>
2019-02-15 10:27:11 +1100

    because I'm slack and didn't commit last time...

--- Modelling animal behavior.R
+++ Modelling animal behavior.R
@@ -16,3 +16,19 @@ weather <- rbind(weather0, weather1)
 
 weather <- replace.value(weather, "Reward", from = -1, to = as.integer(0))
 
+weather <- weather %>%
+  group_by(Day, Subject, Session) %>%
+  mutate(lag.Time = dplyr::lag(Time)) %>%
+  ungroup() %>%
+  mutate(diff.Time = Time - lag.Time) %>%
+  mutate(diff.Time = ifelse(diff.Time < 0, 0, diff.Time))
+
+summary.weather <- weather %>%
+  group_by(Day, Subject, Session) %>%
+  summarise(Reward = sum(Reward, na.rm = TRUE))
+
+rewardplot <- ggplot(summary.weather, aes(x = Day, y = Reward)) +
+  geom_point() +
+  facet_wrap(~Session)
+
+rewardplot
